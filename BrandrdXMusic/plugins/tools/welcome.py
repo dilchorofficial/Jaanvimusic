edcd8b06f6392135b2acdd722cbbcbdca19dedc7
anujjjjjjjjjj <dharamrajput2004@gmail.com>
2025-05-23 19:25:55 +0530

    Update welcome.py

--- BrandrdXMusic/plugins/tools/welcome.py
+++ BrandrdXMusic/plugins/tools/welcome.py
@@ -91,7 +91,7 @@ async def greet_group(_, member: ChatMemberUpdated):
 ๏ 𝗡𝗔𝗠𝗘 ➠ {user.mention}
 ๏ 𝗜𝗗 ➠ {user.id}
 ๏ 𝐔𝐒𝐄𝐑𝐍𝐀𝐌𝐄 ➠ @{user.username}
-๏ 𝐌𝐀𝐃𝐄 𝐁𝐘 ➠ [ᴄʜᴏʀ](https://t.me/Vip_chor)
+๏ 𝐌𝐀𝐃𝐄 𝐁𝐘 ➠ [🤭](https://t.me/ANURAGMOD)
 ➖➖➖➖➖➖➖➖➖➖➖
 """,
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"🥂𝐀𝐃𝐃 𝐌𝐄🥂", url=f"https://t.me/Jaanvi_music_robot?startgroup=true")]])
