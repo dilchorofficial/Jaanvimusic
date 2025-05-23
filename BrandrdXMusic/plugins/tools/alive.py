0cbe73507c53757c13b2688b27ecad62ada18475
anujjjjjjjjjj <dharamrajput2004@gmail.com>
2025-05-23 19:23:45 +0530

    Update alive.py

--- BrandrdXMusic/plugins/tools/alive.py
+++ BrandrdXMusic/plugins/tools/alive.py
@@ -9,21 +9,21 @@ from config import MUSIC_BOT_NAME
 @app.on_message(filters.command(["alive"]))
 async def start(client: Client, message: Message):
     await message.reply_video(
-        video=f"https://graph.org/file/e999c40cb700e7c684b75.mp4",
+        video=f"https://files.catbox.moe/q7h4nt.jpg",
         caption=f"❤️ ʜᴇʏ {message.from_user.mention}\n\n🔮 ɪ ᴀᴍ {MUSIC_BOT_NAME}\n\n✨ ɪ ᴀᴍ ғᴀsᴛ ᴀɴᴅ ᴩᴏᴡᴇʀғᴜʟ ᴍᴜsɪᴄ ᴩʟᴀʏᴇʀ ʙᴏᴛ ᴡɪᴛʜ sᴏᴍᴇ ᴀᴡᴇsᴏᴍᴇ ғᴇᴀᴛᴜʀᴇs.\n\n💫 ɪғ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴛʜᴇɴ ᴊᴏɪɴ ᴏᴜʀ sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ🤍...\n\n━━━━━━━━━━━━━━━━━━❄",
         reply_markup=InlineKeyboardMarkup(
             [
                [
             InlineKeyboardButton(
-                text="☆𝗗꯭ɪʟ꯭c፝֟͠ʜ꯭𝛔ʀ꯭  ", url=f"https://t.me/TEAM_CHOR"
+                text="☆𝗗꯭ɪʟ꯭c፝֟͠ʜ꯭𝛔ʀ꯭  ", url=f"https://t.me/All_super_music"
             ),
             InlineKeyboardButton(
-                text=" 💌 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 💌 ", url=f"https://t.me/+SFB9vAp52SAxZGE1"
+                text=" 💌 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 💌 ", url=f"https://t.me/All_super_music"
             ),
         ],
                 [
             InlineKeyboardButton(
-                text="☆ ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/love_you_jaan_A"
+                text="☆ ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/All_super_music"
             ),
                 ],
                 [
