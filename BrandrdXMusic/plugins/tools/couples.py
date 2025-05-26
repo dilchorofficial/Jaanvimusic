cb7d22a113a87a49debe940689ccd4592b0f4cc4
ANURAG X MUSIC 💞 <lokendrhopal@gmail.com>
2025-05-26 22:40:43 +0530

    Update couples.py

--- BrandrdXMusic/plugins/tools/couples.py
+++ BrandrdXMusic/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from BrandrdXMusic.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="ᴍʏ ᴄᴜᴛᴇ ᴅᴇᴠᴇʟᴏᴘᴇʀ  🥀",
-            url=f"https://t.me/Vip_chor",
+            text="ᴍʏ ᴄᴜᴛᴇ ᴅᴇᴠᴇʟᴏᴘᴇʀ  ",
+            url=f"https://t.me/ANURAGMOD",
         ),
     ],
 ]
