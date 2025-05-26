4f616a4ad01438e12e56f515ac11e654bc6614aa
anujjjjjjjjjj <dharamrajput2004@gmail.com>
2025-05-26 14:54:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,8 +35,9 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/anujjjjjjjjjj/ANURAG-NEW-REPO-TOP",
-)
+    "https://github.com/anujjjjjjjjjj/ANURAG-NEW-REPO-TOP",)
+
+
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
