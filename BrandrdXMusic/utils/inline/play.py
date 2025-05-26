65fe3b3aa2506c835cec1709767d2a2c4b0e98c0
anujjjjjjjjjj <dharamrajput2004@gmail.com>
2025-05-26 12:22:23 +0530

    Update play.py

--- BrandrdXMusic/utils/inline/play.py
+++ BrandrdXMusic/utils/inline/play.py
@@ -69,9 +69,10 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
         [InlineKeyboardButton(text="  ᴀᴅᴅ ᴍᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ ", url=f"https://t.me/MAHI_X_MUSIC_BOT?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users")],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
+    ]
     return buttons
 
-
+    
 def stream_markup(_, videoid, chat_id):
     buttons = [
         [
